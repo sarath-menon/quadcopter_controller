d0701bb03fa40b807b3fb9bbd089372385f6c111
smenon <smenon@ethz.ch>
2021-09-21 21:41:31 +0200

    reorganized folders , not working yet

---
+++ project/include/include_helper.h
@@ -0,0 +1,12 @@
+// #pragma once
+// #include "motor_mixing.h"
+// #include "pid_cascaded.h"
+// #include <chrono>
+// #include <iostream>
+// #include <math.h>
+// #include <math_helper.h>
+// // Fastdds Headers
+// #include "motor_commandsPubSubTypes.h"
+// #include "motor_commandsPublisher.h"
+// // Px4 math header
+// #include "matrix/math.hpp"
