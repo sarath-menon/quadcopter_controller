228c44c964eb15e0be30f4ebec67a7a25e3af83f
smenon <smenon@ethz.ch>
2021-10-09 10:04:52 +0200

    condtion variable wait isntead of repeated for

--- cascaded_controller_app/src/main.cpp
+++ cascaded_controller_app/src/main.cpp
@@ -1,5 +1,28 @@
 #include "include_helper.h"
 
+// Global variables for now
+matrix::Vector<float, 4> motor_commands;
+
+void controller_thread(controllers_2d::BasicPidCascaded &controller,
+                       QuadcopterMixer &mixer, WaypointSetter &target) {
+
+  // Wait until subscriber sends mocap pose
+  std::unique_lock<std::mutex> lk(mocap_sub::m);
+  mocap_sub::cv.wait(lk, [] { return mocap_sub::new_data; });
+
+  // after the wait, controller owns the lock.
+
+  // Set new data flag to false when data received
+  mocap_sub::new_data = false;
+
+  // Cascaded controller
+  matrix::Vector<float, 4> thrust_torque_cmd = controller.cascaded_controller(
+      mocap_sub::position, mocap_sub::orientation_euler, target.position());
+
+  // Convert thrust, torque to motor speeds
+  motor_commands = mixer.motor_mixer(thrust_torque_cmd);
+}
+
 int main() {
 
   // Initialize logger
@@ -49,98 +72,30 @@ int main() {
   target.set_setpoints(paths::setpoint_yaml);
   logger.log_info("Initialized Waypoint setter");
 
-  // Initialize for now
-  matrix::Vector<float, 4> motor_commands;
-  matrix::Vector<float, 4> thrust_torque_cmd;
-
   bool session_end_flag = true;
   logger.log_info("Waiting for mocap datastream");
 
-  int prev_index = 0;
-
   for (;;) {
 
-    if (mocap_sub::new_data == true) {
+    // Start controller thread
+    std::thread ctrl(std::ref(controller_thread), std::ref(controller),
+                     std::ref(mixer), std::ref(target));
 
-      if (mocap_sub::index == 1) {
-        session_end_flag = false;
-        logger.log_info("Starting session");
-      }
+    ctrl.join();
 
-      // logger.log_info("Received pose data");
-      // std::string status_msg = "Received pose data";
-      // safe_cout(status_msg);
+    // Send motor commands to simulator
+    msg.index({mocap_sub::index});
+    msg.motor_commands({motor_commands(0), motor_commands(1), motor_commands(2),
+                        motor_commands(3)});
+    // // Publish motor command msg
+    motor_command_pub.run(msg);
 
-      // Cascaded controller
-      thrust_torque_cmd = controller.cascaded_controller(
-          mocap_sub::position, mocap_sub::orientation_euler, target.position());
+    // if (session_end_flag == false && mocap_sub::matched == 0) {
 
-      // Convert thrust, torque to motor speeds
-      motor_commands = mixer.motor_mixer(thrust_torque_cmd);
-
-      // Send motor commands to simulator
-      msg.index({mocap_sub::index});
-      msg.motor_commands({motor_commands(0), motor_commands(1),
-                          motor_commands(2), motor_commands(3)});
-      // // Publish motor command msg
-      motor_command_pub.run(msg);
-
-      // std::cout << "Published motor commands:" << mocap_sub::index << '\n';
-
-      // std::cout << "Thrust command:" << thrust_torque_cmd(0) << '\n';
-      // std::cout << "Torque commands:" << thrust_torque_cmd(1) << '\t'
-      //           << thrust_torque_cmd(2) << '\t' << thrust_torque_cmd(3) <<
-      //           '\n';
-      // std::cout << "Position:" << mocap_sub::position[0] << '\t'
-      //           << mocap_sub::position[1] << '\t' << mocap_sub::position[2]
-      //           << '\n';
-      // std::cout << "Orientation euler in degrees:"
-      //           << mocap_sub::orientation_euler[0] << '\t'
-      //           << -mocap_sub::orientation_euler[1] << '\t'
-      //           << mocap_sub::orientation_euler[2] << '\n';
-      // std::cout << "Attitude command:" << attitude_command << '\n' << '\n';
-
-      // logger.log_data(mocap_sub::position[0]);
-      // logger.log_data(mocap_sub::position[1]);
-      // logger.log_data(mocap_sub::position[2]);
-      // logger.log_data(mocap_sub::orientation_euler[0]);
-      // logger.log_data(mocap_sub::orientation_euler[1]);
-      // logger.log_data(mocap_sub::orientation_euler[2]);
-      // logger.log_data(thrust_command);
-      // logger.log_data(0);
-      // logger.log_data(torque_command);
-      // logger.log_data(0);
-      // logger.log_data(motor_commands[0]);
-      // logger.log_data(motor_commands[1]);
-      // logger.log_data(motor_commands[2]);
-      // logger.log_data(motor_commands[3]);
-
-      // logger.log_next_line();
-      // logger.add_to_log("thrust_command", &thrust_command);
-      logger.log_now();
-
-      // std::cout << "Motor commands:" << motor_commands[0] << '\t'
-      //           << motor_commands[1] << '\t' << motor_commands[2] << '\t'
-      //           << motor_commands[3] << '\t' << std::endl;
-
-      // std::cout << std::endl;
-
-      // Set flag to false after data has been processed
-      std::lock_guard lk(mocap_sub::m);
-      mocap_sub::new_data = false;
-    }
-
-    else if (session_end_flag == false && mocap_sub::matched == 0) {
-
-      session_end_flag = true;
-      logger.shutdown_data_logger();
-      logger.log_info("Mocap datastreastream has closed. Data log saved");
-      logger.log_info("Waiting for new mocap datastream");
-    }
-
-    else if (session_end_flag == true && mocap_sub::matched == 0) {
-      std::this_thread::sleep_for(std::chrono::seconds(1));
-      std::cout << ".";
-    }
+    //   session_end_flag = true;
+    //   logger.shutdown_data_logger();
+    //   logger.log_info("Mocap datastreastream has closed. Data log saved");
+    //   logger.log_info("Waiting for new mocap datastream");
+    // }
   }
 }
