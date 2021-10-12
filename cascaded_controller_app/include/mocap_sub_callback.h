6e438e91de72471e4f6fd3e2022cb7d8945038c4
smenon <smenon@ethz.ch>
2021-10-12 20:07:44 +0200

    code cleanup

--- cascaded_controller_app/include/mocap_sub_callback.h
+++ cascaded_controller_app/include/mocap_sub_callback.h
@@ -15,14 +15,11 @@ inline void DDSSubscriber::SubListener::on_data_available(DataReader *reader) {
 
   if (reader->take_next_sample(&sub::msg, &info) == ReturnCode_t::RETCODE_OK) {
     if (info.valid_data) {
-      { // Protection against race condition using mutex
-        std::unique_lock<std::mutex> lock(m);
-
-        // std::cout << "Sample received, count=" << samples << std::endl;
-
+      // std::cout << "Sample received, count=" << samples << std::endl;
+      {
+        // Protection against race condition using mutex
+        std::lock_guard lock(m);
         sub::new_data_flag = true;
-        // std::cout << "subscriber signals data ready for processing\n";
-        lock.unlock();
       }
       cv.notify_one();
     }
